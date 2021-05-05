a0bfdbe3c1f9f2d271e86b20d51936752e7718fc
MengChiaLee <supertl0624@gmail.com>
2021-05-05 16:01:46 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import SignInSide from './SignIn.js';
+function App() {
+  return (
+    <div className="App">
+    {SignInSide()}
+    </div>
+  );
+}
+
+export default App;
